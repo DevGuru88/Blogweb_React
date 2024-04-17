3147a6954b4a2518313f3b0b4260357157c982e3
Soaham <113767354+soaham-pimparkar@users.noreply.github.com>
2024-04-18 00:51:45 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,33 @@
+import Select from "./Fileselect";
+import Header from "./Folderheader/Header";
+import Footer from "./Footer/Footer";
+import Container from "./Foldercontainer/Filecontainer";
+import Logo from "./Filelogo";
+import {LogoutBtn} from "./Folderheader/FilelogoutBtn";
+//import RTE from "./FileRTE";
+import {Signup} from "./FileSignup";
+import {Login} from "./Filelogin";
+import Button from "./Filebutton";
+import PostForm from "./post-form/Filepostform";
+import {PostCard} from "./Filepostcard";
+
+import {AuthLayout} from "./Fileauthlayout.jsx";
+import {Input} from "./Fileinput";
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    //RTE,
+    Signup,
+    Login,
+    Button,
+    PostForm,
+    PostCard,
+    AuthLayout, 
+    Input,
+    Select,
+}
+
+
